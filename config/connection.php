dc86530154bbaeef3b47f97a0b029a56e975ed34
vbpawar <vikaspawar3110@gmail.com>
2019-07-20 15:42:49 +0530

    added htaccess

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,5 @@ $servername = 'localhost';
 $username   = 'root';
 $password   = '';
 $dbname     = 'xxovek_sales';
-
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error($con));
-
 ?>
